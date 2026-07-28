08d47ff36775b289cc80a493168b982c783a1c78
ujjwal <ugupta17k@gmail.com>
2026-07-28 22:33:16 +0530

    Home page completed

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -12,9 +12,9 @@ export const HomePage = ()=>{
     return ( 
         <div className=" w-full h-screen sm:px-[15%] sm:py-[5%]   ">
             <div className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${SelectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'} `}>
-                <SideBar />
-                <ChatContainer /> 
-                <RightSideBar />
+                <SideBar SelectedUser={SelectedUser} setSelectedUser={setSelectedUser} />
+                <ChatContainer SelectedUser={SelectedUser} setSelectedUser={setSelectedUser}  /> 
+                <RightSideBar SelectedUser={SelectedUser} />
             </div>
         </div>
     )
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatMessgetime(date){
+  return new Date(date).toLocaleTimeString("en-US", {
+    hour : "2-digit",
+    minute : "2-digit",
+    hour12 : false
+  })
+}
